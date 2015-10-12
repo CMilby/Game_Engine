a5086d1f2dfbb79f7e9de8cc72c938967b7c7442
CMilby <cmilby99@gmail.com>
2015-10-12 10:27:58 -0400

    Initial Commit

---
+++ Game_Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game_Engine
+//
+//  Created by Craig Milby on 10/12/15.
+//  Copyright © 2015 Craig Milby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
